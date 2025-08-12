cd41bc2e52fce9e2e68d26246f599bf6a49a57e0
pytlakow <pytlakow@gmail.com>
2025-08-12 15:58:39 +0200

    config for vercel

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: isProd ? 'export' : undefined,
-  distDir: isProd ? 'docs' : undefined,
-  basePath: isProd ? '/lz-marketing' : undefined,
-  assetPrefix: isProd ? '/lz-marketing' : undefined,
-  reactStrictMode: true,
+  // output: isProd ? 'export' : undefined,
+  // distDir: isProd ? 'docs' : undefined,
+  // basePath: isProd ? '/lz-marketing' : undefined,
+  // assetPrefix: isProd ? '/lz-marketing' : undefined,
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
